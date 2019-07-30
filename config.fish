6c6eb91b41e9c427c903780a919e6cd8af8f2178
ertugrul <ertugrul013@hotmail.com>
2019-07-30 12:14:34 +0200

    init push

---
+++ config.fish
@@ -0,0 +1,32 @@
+export ANDROID_HOME=$HOME/Android/Sdk
+
+export PATH="$PATH:/home/eddie-veteran/Applications/flutter/flutter/bin"
+
+export PATH="$PATH":"$HOME/Applications/flutter/flutter/.pub-cache/bin"
+
+export PATH="$PATH":"/usr/lib/dart/bin"
+
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+
+alias c="clear"
+alias wifi="nmtui"
+
+alias gits="git status -s"
+alias gitp="git push"
+alias gitc="git commit"
+alias gita="git add"
+alias gitA="git add -A"
+alias gitall="git add -A ; git commit; git push"
+alias gitr="git remote add origin"
+alias gitpo="git push -u origin master"
+alias gitl="git log --graph --abbrev-commit --decorate --date=relative --all"
+
+#pictue in picture mode MPV
+alias pip="mpv --ontop --no-border --on-all-workspaces --autofit=634x366 --geometry=99%:2%"
+
+#spacefish
+set SPACEFISH_TIME_SHOW false
+set SPACEFISH_DATE_SHOW	false
+
+#welcome message
+cowsay -w "SLEEP I DON'T NEED SLEEP"
